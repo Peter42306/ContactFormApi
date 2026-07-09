29caed18290d53a121a064f8f7a824dae852d8ed
Petr Zalizko <pzalizko@gmail.com>
2026-07-09 23:10:47 +0300

    Implement contact form application service

---
+++ ContactFormApi.Application/Services/ContactFormService.cs
@@ -0,0 +1,68 @@
+﻿using ContactFormApi.Application.DTOs.Contact;
+using ContactFormApi.Application.Interfaces.Repositories;
+using ContactFormApi.Application.Interfaces.Services;
+using ContactFormApi.Domain.Entities;
+using ContactFormApi.Domain.Enums;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactFormApi.Application.Services
+{
+    public class ContactFormService : IContactFormService
+    {
+        private readonly IValidator<ContactFormRequestDto> _validator;
+        private readonly IContactMessageRepository _repository;
+        private readonly IEmailSender _emailSender;
+
+        public ContactFormService(
+            IValidator<ContactFormRequestDto> validator,
+            IContactMessageRepository repository,
+            IEmailSender emailSender)
+        {
+            _validator = validator;
+            _repository = repository;
+            _emailSender = emailSender;
+        }
+
+        public async Task<ContactFormResponseDto> SendAsync(
+            ContactFormRequestDto request,
+            CancellationToken ct = default)
+        {
+            var validationResult = await _validator.ValidateAsync(request, ct);
+            if (!validationResult.IsValid)
+            {
+                return new ContactFormResponseDto
+                {
+                    Success = false,
+                    Message = "Invalid contact form data"
+                };
+            }
+
+            var contactMessage = new ContactMessage
+            {
+                Id = Guid.NewGuid(),
+                AppKey = request.AppKey,
+                AppName = request.AppKey,
+                SenderName = request.SenderName,
+                SenderEmail = request.SenderEmail,
+                Subject = request.Subject,
+                Body = request.Body,
+                Status = ContactMessageStatus.Pending,
+                CreatedAtUtc = DateTime.UtcNow
+            };
+
+            await _repository.AddAsync(contactMessage, ct);
+
+            return new ContactFormResponseDto
+            {
+                MessageId = contactMessage.Id,
+                Success = true,
+                Message = "Contact message received successfully."
+            };
+        }
+    }
+}
---
+++ ContactFormApi.Application/Validators/Contact/ContactFormRequestDtoValidator.cs
@@ -0,0 +1,37 @@
+﻿using ContactFormApi.Application.DTOs.Contact;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactFormApi.Application.Validators.Contact
+{
+    public class ContactFormRequestDtoValidator : AbstractValidator<ContactFormRequestDto>
+    {
+        public ContactFormRequestDtoValidator()
+        {
+            RuleFor(x => x.AppKey)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.SenderName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.SenderEmail)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(200);
+
+            RuleFor(x => x.Subject)
+                .NotEmpty()
+                .MaximumLength(200);
+
+            RuleFor(x => x.Body)
+                .NotEmpty()
+                .MaximumLength(5000);
+        }
+    }
+}
